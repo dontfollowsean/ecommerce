24713612caf6354346750032b477626473d617ad
Sean Wilkinson <sean@infotechsoft.com>
2016-06-13 14:30:56 -0400

    modified .gitignore added mongoose and bcrypt dependencies

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
 var	port = 3000;
+
+mongoose.connect('', function (err) {
+    if (err){
+        console.log(err);
+    } else {
+        console.log('Connected to database');
+    }
+});
 
 //Middleware used for logging
 app.use(morgan('dev'));
